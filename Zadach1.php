8979da444b7eba875024581c6f4bc8292a94c154
Artyom <Batonovmisa@gmail.com>
2025-03-08 11:29:56 +0300

    88

---
+++ Zadach1.php
@@ -0,0 +1,6 @@
+<?php
+$Ocenka = [3, 2, 3, 5, 4, 2, 2, 3, 4, 5, 2];
+$Sum = array_sum($Ocenka);
+$Count  = Count($grades);
+$Average = $Sum / $Count;
+echo "Средняя оценка:" . round($average, 2) . "\n";
