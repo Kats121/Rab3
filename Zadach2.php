b94456d00a070313fec005064c383d7b68c68f98
Artyom <Batonovmisa@gmail.com>
2025-03-09 19:48:25 +0300

    72

--- Zadach2.php
+++ Zadach2.php
@@ -2,9 +2,8 @@
 $Massiv = [52, 85, 76, 88, 78, 78, 54, 66];
 $DeleteNum =  array_unique($Massiv);
 rsort($DeleteNum);
-if (count($Numbers) >= 3) {
-    $Max = $Numbers[2];
+if (count($Massiv) >= 3) {
+    $Max = $Massiv[2];
     echo "Третье максимальное число: $Max\n";
 } else {
-    echo "В массиве недостаточно уникальных чисел.\n";
 }
